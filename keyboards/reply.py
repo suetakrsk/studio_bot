59a2b5e5e73a7b77c85e7ef1270c6549259211f0
Grisha Kuznetsov <awtershokk@gmail.com>
2023-07-10 17:59:28 +0700

    СОЗДАНИЕ БД

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,11 +1,13 @@
 from telebot import types
 def create_reply_keyboard():
     main_keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+
     create_order_button = "🛠 Создать заказ"
     my_orders_button = "👤 Мои заказы"
-    hz_knopka = "❓❓❓"
+    feedback_button = "📞 Обратная связь"
     reviews_button = "📝 Отзывы"
+
     main_keyboard.add(create_order_button, my_orders_button)
-    main_keyboard.add(hz_knopka, reviews_button)
+    main_keyboard.add(feedback_button, reviews_button)
 
     return main_keyboard
