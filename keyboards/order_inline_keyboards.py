567485db4bbf74663f07443cc866f6433544a82f
Grisha Kuznetsov <awtershokk@gmail.com>
2023-07-11 02:21:15 +0700

    убрал говнокод, сделал нормальные имена для файлов

---
+++ keyboards/order_inline_keyboards.py
@@ -0,0 +1,16 @@
+from telebot import types
+def select_order_category(message, bot):
+    select_category_keyboard = types.InlineKeyboardMarkup()
+    diplom_button = types.InlineKeyboardButton('Дипломная работа', callback_data='diplom')
+    kurs_button= types.InlineKeyboardButton('Курсовая работа', callback_data='kursovaya')
+    practika_button = types.InlineKeyboardButton('Практика(УП/ПП)', callback_data='practika')
+    lab_button = types.InlineKeyboardButton('Лабораторная работа', callback_data='laborator')
+    another_button = types.InlineKeyboardButton('Другое', callback_data='another')
+
+    select_category_keyboard.add(diplom_button)
+    select_category_keyboard.add(kurs_button)
+    select_category_keyboard.add(practika_button)
+    select_category_keyboard.add(lab_button)
+    select_category_keyboard.add(another_button)
+
+    bot.send_message(message.chat.id, '⏳ Выберите категорию работы:', reply_markup=select_category_keyboard)
--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import os
 from dotenv import load_dotenv
 from handlers.start_handler import start_handler
 from handlers.help_handler import help_handler
-from keyboards.reply import create_reply_keyboard
-from handlers.create_order import select_order_category
-from handlers.create_order import get_selected_category
-from handlers.create_order import get_selected_type
-from handlers.create_order import get_selected_deadline
+from keyboards.main_reply_keyboard import create_reply_keyboard
+
 
 
 load_dotenv()
@@ -18,18 +15,6 @@ bot = telebot.TeleBot(token)
 def handle_start(message):
     main_keyboard = create_reply_keyboard()
     start_handler(message, bot, main_keyboard)
-
-@bot.callback_query_handler(func=lambda call: call.data in ['diplom', 'kursovaya', 'practika', 'laborator', 'another'])
-def handle_order_category(call):
-    get_selected_category(call, bot)
-
-@bot.callback_query_handler(func=lambda call: call.data in ['site', 'consol', 'dekstop', 'telegram_bot', 'another'])
-def handle_order_type(call):
-    get_selected_type(call, bot)
-
-@bot.callback_query_handler(func=lambda call: call.data in ['<1_week', '1-2_week', '2_week-1_month', '1-2_month', '2_month_and_more'])
-def handle_order_deadline(call):
-    get_selected_deadline(call, bot)
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
