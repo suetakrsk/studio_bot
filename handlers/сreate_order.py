2321c05108a18a1def9f6292f392444814d87395
mirout <maksimka.prokopets@mail.ru>
2023-07-12 13:43:17 +0700

    11

--- handlers/сreate_order.py
+++ handlers/сreate_order.py
@@ -1,6 +1,6 @@
 from keyboards.order_inline_keyboards import select_order_type, select_order_deadline
 
-infO_product = {
+info_product = {
     'Категория работы': None,
     'Тип работы': None,
     'Сроки': None,
@@ -24,36 +24,36 @@ deadline_text = {
     'min1week': ['Меньше 1 недели', 2],
     'odna_dve_week': ['1-2 недели', 1.5],
     'two_week_month': ['2 недели - 1 месяц', 1.2],
-    'month': ['1-2 месяца', 1],
+    'month-twomonth': ['1-2 месяца', 1],
     'two-month_more': ['2 месяца и более', 0.9]
 }
 
 def choise_order_category(call, bot):
-    global infO_product
+    global info_product
     global categoru_texts
     category = call.data
-    infO_product['Категория работы'] = categoru_texts[category][0]
-    infO_product['Расчетная стоимость'] = categoru_texts[category][1]
+    info_product['Категория работы'] = categoru_texts[category][0]
+    info_product['Расчетная стоимость'] = categoru_texts[category][1]
 
 
 def choise_order_type(call, bot):
-    global infO_product
+    global info_product
     global type_texts
     types = call.data
-    infO_product['Тип работы'] = type_texts[types]
+    info_product['Тип работы'] = type_texts[types]
 
 
 def choise_order_deadline(call, bot):
-    global infO_product
+    global info_product
     global deadline_text
     category = call.data
-    infO_product['Сроки'] = deadline_text[category][0]
-    infO_product['Расчетная стоимость'] *= deadline_text[category][1]
+    info_product['Сроки'] = deadline_text[category][0]
+    info_product['Расчетная стоимость'] *= deadline_text[category][1]
 
 def last_craete_order_message(call, bot):
-    global infO_product
+    global info_product
     message = f"""
-    Ваш заказ:\n№ заказа - #1\nКатегория работы - {infO_product['Категория работы']}\nТип работы - {infO_product['Тип работы']}\nСроки - {infO_product['Сроки']}\nРасчетная стоимость - {infO_product['Расчетная стоимость']}\n\n*- Окончательная стоимость - (добавляется после подтверждения заказа у администратора)
+    Ваш заказ:\n№ заказа - #1\nКатегория работы - {info_product['Категория работы']}\nТип работы - {info_product['Тип работы']}\nСроки - {info_product['Сроки']}\nРасчетная стоимость - {info_product['Расчетная стоимость']}\n\n*- Окончательная стоимость - (добавляется после подтверждения заказа у администратора)
     """
     bot.send_message(call.message.chat.id, text=message)
 
